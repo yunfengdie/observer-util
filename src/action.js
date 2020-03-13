bf72b6958572881ce1feb3ea003c55e1b3f17b70
paranoidjk <hust2012jiangkai@gmail.com>
2020-03-13 17:46:59 +0800

    lint

--- src/action.js
+++ src/action.js
@@ -1,41 +1,41 @@
-import { InternalConfig } from './config';
-import { createTransaction } from './transaction';
-import { StackManager } from './utils/stack';
-import { decoratorFactory } from './utils/decorator';
+import { InternalConfig } from './config'
+import { createTransaction } from './transaction'
+import { StackManager } from './utils/stack'
+import { decoratorFactory } from './utils/decorator'
 
-export const action = decoratorFactory(createAction);
-export const actionManager = new StackManager();
+export const action = decoratorFactory(createAction)
+export const actionManager = new StackManager()
 
-function canWrite() {
-  return !InternalConfig.onlyAllowChangeInAction || actionManager.duringStack;
+function canWrite () {
+  return !InternalConfig.onlyAllowChangeInAction || actionManager.duringStack
 }
 
 export const DISABLE_WRITE_ERR =
-  '[nemo-observable-util] can not modify data outside @action';
-export function writeAbleCheck() {
+  '[nemo-observable-util] can not modify data outside @action'
+export function writeAbleCheck () {
   if (!canWrite()) {
-    throw new Error(DISABLE_WRITE_ERR);
+    throw new Error(DISABLE_WRITE_ERR)
   }
 }
 
-function createAction(originalFunc) {
+function createAction (originalFunc) {
   if (typeof originalFunc !== 'function') {
     throw new Error(
       'action should must wrap on Function: ' + typeof originalFunc
-    );
+    )
   }
-  const transactionFn = createTransaction(originalFunc);
-  const identity = actionManager.getUUID();
-  return function(...args) {
-    actionManager.start(identity);
+  const transactionFn = createTransaction(originalFunc)
+  const identity = actionManager.getUUID()
+  return function (...args) {
+    actionManager.start(identity)
     try {
-      return transactionFn.apply(this, args);
+      return transactionFn.apply(this, args)
     } finally {
-      actionManager.end(identity);
+      actionManager.end(identity)
     }
-  };
+  }
 }
 
-export function runInAction(fn) {
-  return action(fn)();
+export function runInAction (fn) {
+  return action(fn)()
 }
